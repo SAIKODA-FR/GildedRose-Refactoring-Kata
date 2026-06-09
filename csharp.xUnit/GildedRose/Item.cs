94fbe5dabb7012c535790bc93bdbee9094204c0e
Vincent Bourgeois <vincebourgeois@gmail.com>
2026-06-09 17:42:37 +0200

    polymor Backstage

--- csharp.xUnit/GildedRose/Item.cs
+++ csharp.xUnit/GildedRose/Item.cs
@@ -1,37 +1,8 @@
 ﻿namespace GildedRoseKata;
 
-public class Item : IItem
+public class Item(string name, int sellIn, int quality) : BaseItem(name,sellIn, quality)
 {
-    public string Name { get; set; }
-    public int SellIn { get; set; }
-    public int Quality { get; set; }
-
-    public Item(string name, int sellIn, int quality)
-    {
-        Name = name;
-        SellIn = sellIn;
-        Quality = quality;
-    }
-
-    private void ResetQuality()
-    {
-        Quality = 0;
-    }
-
-    private void DecreaseQuality()
-    {
-        if (Quality > 0)
-        {
-            Quality = Quality - 1;
-        }
-    }
-
-    private void DecreaseSellin()
-    {
-        SellIn = SellIn - 1;
-    }
-
-    public void UpdateQuality()
+    public override void UpdateQuality()
     {
         DecreaseQuality();
 
