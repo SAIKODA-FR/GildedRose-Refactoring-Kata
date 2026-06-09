d85cff567e2586059dc93d2280ddb9a38c74a636
Vincent Bourgeois <vincebourgeois@gmail.com>
2026-06-09 16:48:41 +0200

    Move into item

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -4,11 +4,6 @@ namespace GildedRoseKata;
 
 public class GildedRose
 {
-    private const string AgedBrie = "Aged Brie";
-    private const string BackStage = "Backstage passes to a TAFKAL80ETC concert";
-    private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
-    private const int MaxQuality = 50;
-
     IList<Item> Items;
 
     public GildedRose(IList<Item> Items)
@@ -20,88 +15,10 @@ public class GildedRose
     {
         foreach (var item in Items)
         {
-            if (item.Name == Sulfuras)
-            {
-                continue;
-            }
-            if (item.Name == AgedBrie)
-            {
-                UpdateQualityAndSellinBrie(item);
-                continue;
-            }
-            if (item.Name == BackStage)
-            {
-                UpdateQualityAndSellinBackStage(item);
-                continue;
-            }
-            UpdateQualityAndSellin(item);
+            item.UpdateQuality();
         }
     }
 
-    private void UpdateQualityAndSellinSulfuras(Item item)
-    {
-
-    }
-
-    private void UpdateQualityAndSellin(Item item)
-    {
-        item.DecreaseQuality();
-
-        item.DecreaseSellin();
-
-        if (item.SellIn < 0)
-        {
-            item.DecreaseQuality();
-        }
-    }
-
-    private void UpdateQualityAndSellinBackStage(Item item)
-    {
-        if (item.Quality < MaxQuality)
-        {
-            item.IncreaseQuality();
-
-            if (item.SellIn < 11)
-            {
-                if (item.Quality < MaxQuality)
-                {
-                    item.IncreaseQuality();
-                }
-            }
-
-            if (item.SellIn < 6)
-            {
-                if (item.Quality < MaxQuality)
-                {
-                    item.IncreaseQuality();
-                }
-            }
-        }
-
-        item.DecreaseSellin();
-
-        if (item.SellIn < 0)
-        {
-            item.ResetQuality();
-        }
-    }
-
-    private void UpdateQualityAndSellinBrie(Item item)
-    {
-        if (item.Quality < MaxQuality)
-        {
-            item.IncreaseQuality();
-        }
-
-        item.DecreaseSellin();
-
-        if (item.SellIn < 0)
-        {
-            if (item.Quality < MaxQuality)
-            {
-                item.IncreaseQuality();
-            }
-        }
-    }
+    
 
 }
--- csharp.xUnit/GildedRose/Item.cs
+++ csharp.xUnit/GildedRose/Item.cs
@@ -2,6 +2,11 @@
 
 public class Item
 {
+    private const string AgedBrie = "Aged Brie";
+    private const string BackStage = "Backstage passes to a TAFKAL80ETC concert";
+    private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+    private const int MaxQuality = 50;
+
     public string Name { get; set; }
     public int SellIn { get; set; }
     public int Quality { get; set; }
@@ -27,5 +32,90 @@ public class Item
     public void IncreaseQuality()
     {
         Quality = Quality + 1;
+    }
+
+    public void UpdateQuality()
+    {
+        if (Name == Sulfuras)
+        {
+            return;
+        }
+        if (Name == AgedBrie)
+        {
+            UpdateQualityAndSellinBrie();
+            return;
+        }
+        if (Name == BackStage)
+        {
+            UpdateQualityAndSellinBackStage();
+            return;
+        }
+        UpdateQualityAndSellin();
+    }
+
+    private void UpdateQualityAndSellinSulfuras()
+    {
+
+    }
+
+    private void UpdateQualityAndSellin()
+    {
+        DecreaseQuality();
+
+        DecreaseSellin();
+
+        if (SellIn < 0)
+        {
+            DecreaseQuality();
+        }
+    }
+
+    private void UpdateQualityAndSellinBackStage()
+    {
+        if (Quality < MaxQuality)
+        {
+            IncreaseQuality();
+
+            if (SellIn < 11)
+            {
+                if (Quality < MaxQuality)
+                {
+                    IncreaseQuality();
+                }
+            }
+
+            if (SellIn < 6)
+            {
+                if (Quality < MaxQuality)
+                {
+                    IncreaseQuality();
+                }
+            }
+        }
+
+        DecreaseSellin();
+
+        if (SellIn < 0)
+        {
+            ResetQuality();
+        }
+    }
+
+    private void UpdateQualityAndSellinBrie()
+    {
+        if (Quality < MaxQuality)
+        {
+            IncreaseQuality();
+        }
+
+        DecreaseSellin();
+
+        if (SellIn < 0)
+        {
+            if (Quality < MaxQuality)
+            {
+                IncreaseQuality();
+            }
+        }
     }
 }
