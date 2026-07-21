4563ec4e638147f381e6d196b525eb640e973e99
Vincent Bourgeois <vincebourgeois@gmail.com>
2026-07-21 14:53:46 +0200

    convert for to foreach

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -17,74 +17,74 @@ public class GildedRose
 
     public void UpdateQuality()
     {
-        for (var i = 0; i < Items.Count; i++)
+        foreach (Item item in Items)
         {
-            if (Items[i].Name != AGED_BRIE && Items[i].Name != BACKSTAGE)
+            if (item.Name != AGED_BRIE && item.Name != BACKSTAGE)
             {
-                if (Items[i].Quality > 0)
+                if (item.Quality > 0)
                 {
-                    if (Items[i].Name != SULFURAS)
+                    if (item.Name != SULFURAS)
                     {
-                        Items[i].Quality = Items[i].Quality - 1;
+                        item.Quality = item.Quality - 1;
                     }
                 }
             }
             else
             {
-                if (Items[i].Quality < 50)
+                if (item.Quality < 50)
                 {
-                    Items[i].Quality = Items[i].Quality + 1;
+                    item.Quality = item.Quality + 1;
 
-                    if (Items[i].Name == BACKSTAGE)
+                    if (item.Name == BACKSTAGE)
                     {
-                        if (Items[i].SellIn < 11)
+                        if (item.SellIn < 11)
                         {
-                            if (Items[i].Quality < 50)
+                            if (item.Quality < 50)
                             {
-                                Items[i].Quality = Items[i].Quality + 1;
+                                item.Quality = item.Quality + 1;
                             }
                         }
 
-                        if (Items[i].SellIn < 6)
+                        if (item.SellIn < 6)
                         {
-                            if (Items[i].Quality < 50)
+                            if (item.Quality < 50)
                             {
-                                Items[i].Quality = Items[i].Quality + 1;
+                                item.Quality = item.Quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (Items[i].Name != SULFURAS)
+            if (item.Name != SULFURAS)
             {
-                Items[i].SellIn = Items[i].SellIn - 1;
+                item.SellIn = item.SellIn - 1;
             }
 
-            if (Items[i].SellIn < 0)
+            if (item.SellIn < 0)
             {
-                if (Items[i].Name != AGED_BRIE)
+                if (item.Name != AGED_BRIE)
                 {
-                    if (Items[i].Name != BACKSTAGE)
+                    if (item.Name != BACKSTAGE)
                     {
-                        if (Items[i].Quality > 0)
+                        if (item.Quality > 0)
                         {
-                            if (Items[i].Name != SULFURAS)
+                            if (item.Name != SULFURAS)
                             {
-                                Items[i].Quality = Items[i].Quality - 1;
+                                item.Quality = item.Quality - 1;
                             }
                         }
                     }
                     else
                     {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        item.Quality = item.Quality - item.Quality;
                     }
                 }
                 else
                 {
-                    if (Items[i].Quality < 50)
+                    if (item.Quality < 50)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        item.Quality = item.Quality + 1;
                     }
                 }
             }
