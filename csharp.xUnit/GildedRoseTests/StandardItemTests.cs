bb03445a063c22aac44fd4e0169295b170dafa07
Vincent Bourgeois <vincebourgeois@gmail.com>
2026-07-22 10:55:25 +0200

    Added assemblyIndfo with internalVisibleTo

---
+++ csharp.xUnit/GildedRoseTests/StandardItemTests.cs
@@ -0,0 +1,18 @@
+﻿using GildedRoseKata;
+using Xunit;
+
+namespace GildedRoseTests
+{
+    public class StandardItemTests
+    {
+        [Fact]
+        public void Sellin_decreases_each_day()
+        {
+            var standardItem = new StandardItem("testName", 0, 0);
+
+            standardItem.UpdateQuality();
+
+            Assert.Equal(-1, standardItem.SellIn);
+        }
+    }
+}
